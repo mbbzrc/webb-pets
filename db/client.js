bab60b9e6b51eda918ed853b47dbaf38a6bf484e
mbbzrc <antoinebeesley@gmail.com>
2021-08-21 12:33:06 -0700

    refactored DB client code

--- db/client.js
+++ db/client.js
@@ -1,6 +1,5 @@
 const { Client } = require("pg");
-const DB_NAME = "localhost:5432/data-moguls";
-const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
-const client = new Client(DB_URL);
+
+const client = new Client(process.env.DATABASE_URL);
 
 module.exports = { client };

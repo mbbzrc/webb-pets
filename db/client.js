13ec03277f67367cf30eddd24b4dc8a91f638fe4
mbbzrc <antoinebeesley@gmail.com>
2021-08-21 13:46:59 -0700

    heroku update

--- db/client.js
+++ db/client.js
@@ -3,9 +3,9 @@ const { Client } = require("pg");
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl:
-    process.env.NODE_ENV === "production" || process.env.NODE_ENV === "sandbox"
-      ? true
-      : false,
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = { client };

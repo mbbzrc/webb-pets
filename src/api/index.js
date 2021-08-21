2f887c00e40f63bdeba275a2175e93a7bedc79bb
mbbzrc <antoinebeesley@gmail.com>
2021-08-20 19:27:00 -0700

    formatting

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = "http://localhost:3000";
 // update once deployed
 
 export async function getProductById(id) {
@@ -14,7 +14,7 @@ export async function getProductById(id) {
 
 export async function getAllProducts() {
   try {
-    const {data} = await axios.get(`${BASE_URL}/api/products`);
+    const { data } = await axios.get(`${BASE_URL}/api/products`);
     return data;
   } catch (error) {
     throw error;

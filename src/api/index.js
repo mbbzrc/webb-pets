6ea5e35d024cccf1027f73328686933b698c1b80
mbbzrc <antoinebeesley@gmail.com>
2021-08-20 20:21:45 -0700

    fixes from PR review

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://localhost:5000";
 // update once deployed
 
 export async function getProductById(id) {

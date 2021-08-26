f1ebda43ebc140303104a1fa0c1df3533b896cc6
beatrice <beatricewhitee@gmail.com>
2021-08-25 19:25:11 -0700

    moved files into routes

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,10 @@ apiRouter.use('/health', healthRouter);
 const productsRouter = require('./products');
 apiRouter.use('/products', productsRouter);
 
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
+
+const ordersRouter = require('../../routes/orders');
+apiRouter.use('/orders', ordersRouter);
+
 module.exports = apiRouter;

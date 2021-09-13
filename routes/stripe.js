b400f262bc2ba48df3985126ffa20f93ae1fe84f
srgburns89 <72822640+srgburns89@users.noreply.github.com>
2021-09-12 19:01:38 -0700

    UPDATED

--- routes/stripe.js
+++ routes/stripe.js
@@ -6,6 +6,7 @@ const stripe = require("stripe")(STRIPE_SECRET_KEY);
 stripeRouter.post("/pay", async (req, res, next) => {
   try {
     const charge = await stripe.charges.create(req.body);
+    console.log('This is the CHARGE', charge)
     res.send({ success: charge });
 
     res.send({ charge });

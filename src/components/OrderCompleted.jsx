456c74ff8cc2110f0dcea9e1072854c44a5859cb
srgburns89 <72822640+srgburns89@users.noreply.github.com>
2021-09-13 20:22:13 -0700

    updated

--- src/components/OrderCompleted.jsx
+++ src/components/OrderCompleted.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 import { formatCurrency } from "../api";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
-    margin: '1rem',
+    margin: "1rem",
   },
   media: {
     height: 140,
@@ -26,52 +26,51 @@ export const OrderCompleted = ({ openOrder }) => {
 
   return (
     <>
-    <div className="my-cart">
-  <h2 className="order-title">Order #{openOrder.orderId}</h2>
-      <p>Date placed: {openOrder.datePlaced}</p>
-      <div className="cart-items">
-      {orderProducts.length > 0 &&
-        orderProducts.map(
-          ({ name, price, quantity, imageURL, orderProductId, index }) => {
-            return (
-              <div className="product" key={orderProductId}>
-                  <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={imageURL}
-        />
-        <CardContent>
-         
-          <h2 style={{textAlign: 'center'}}>{name}</h2>
-          
-          <Typography variant="body2" color="textSecondary" component="p">
-          Quantity: {quantity}
-          
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          Item price: {formatCurrency(price)}
-          
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          Product subtotal: ${(price * quantity).toFixed(2)}
-          
-          </Typography>
-          
-          
-        </CardContent>
-      </CardActionArea>
+      <div className="my-cart">
+        <h2 className="order-title">Order #{openOrder.orderId}</h2>
+        <p>Date placed: {openOrder.datePlaced}</p>
+        <div className="cart-items">
+          {orderProducts.length > 0 &&
+            orderProducts.map(
+              ({ name, price, quantity, imageURL, orderProductId, index }) => {
+                return (
+                  <div className="product" key={orderProductId}>
+                    <Card className={classes.root}>
+                      <CardActionArea>
+                        <CardMedia className={classes.media} image={imageURL} />
+                        <CardContent>
+                          <h2 style={{ textAlign: "center" }}>{name}</h2>
 
-    </Card>
-
-
-
-              </div>
-            );
-          }
-        )}
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                          >
+                            Quantity: {quantity}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                          >
+                            Item price: {formatCurrency(price)}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                          >
+                            Product subtotal: ${(price * quantity).toFixed(2)}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </div>
+                );
+              }
+            )}
         </div>
-        </div>
+      </div>
     </>
   );
 };
